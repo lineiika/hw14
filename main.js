adcc93fb29d00c65bc3469f1f993a417cc3aea4c
lineiika <avramenko.liana@gmail.com>
2023-02-02 09:51:40 +0200

    add commit 2/02

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-let num = 0;
-function sum(number) {
-  let a = number;
-  sum= num+a
-  console.log(sum)
-    function sum1(number) {
-      let b = number;
-      sum1 = sum+b
-      console.log(sum1);
-    }
-    function sum2(number) {
-      let c = number;
-      sum2 = c+sum1
-      console.log(sum2);
-    }
-    sum1(5);
-    sum2(4);
-}
-sum(5);
+
+let sum =(function(){
+  let a = 0;
+  return function (number){
+    return a = a + number;
+  }
+})();
+
+console.log(sum(2));
+console.log(sum(3));
+console.log(sum(20));
+
+
+
+
+
+
+
+ 
